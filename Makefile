85a107a4887a75eab5ad9b6757103a5a407f9e97
fsossai <federico.sossai@gmail.com>
2023-02-23 13:26:05 -0600

    Structure

---
+++ Makefile
@@ -0,0 +1,6 @@
+compile:
+	cmake -S . -B build
+	cmake --build build
+
+clean:
+	rm -rf build
---
+++ lib/tracker.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+namespace wawtrack {
+
+void version(void) {
+  cout << "wawtrack version 0.1.0" << endl;
+}
+
+} // namespace wawtrack
